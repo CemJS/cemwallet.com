395a45baa689d5e6605bd1baacd737a50c8f0bf7
Konstantin <s4etmc@yandex.ru>
2023-12-21 12:46:01 +0300

    fix preloader

--- services/preloader/index.ts
+++ services/preloader/index.ts
@@ -1,5 +1,7 @@
 const app = document.getElementById('app');
 const preloader = document.getElementById('preloader');
+const percent = document.querySelector('.preloader_percent');
+const prog = document.getElementById('progress');
 
 export const loader = async function (Variable: any) {
     Variable.$el = {
@@ -8,8 +10,26 @@ export const loader = async function (Variable: any) {
     }
 }
 
-export const progress = async function () {
-    if (preloader) {
-        preloader.style.display = "none"
+export const progress = async function ({ load, total }) {
+
+    let count = Math.round((load / total) * 100)
+    if (percent && prog) {
+        if (count == 100) {
+            percent.classList.add("text_blink");
+            percent.textContent = 'Connected to CEM Wallet!';
+            setTimeout(() => {
+                if (preloader) {
+                    preloader.style.display = "none"
+                }
+            }, 50);
+        } else {
+            prog.style.width = 200 / 100 * count + 'px';
+            if (count > 80) {
+                percent.textContent = 'Connected to CEM Wallet!';
+            } else {
+                percent.textContent = count + '%';
+
+            }
+        }
     }
 }
